896df614a6e6f810ace12265f156bf58728a0b3c
Leonard Myovela <leonardmvl@gmail.com>
2026-08-31 12:43:18 +0200

    Wrangling Pp and Fd.R

--- Wrangling Pp and Fd.R
+++ Wrangling Pp and Fd.R
@@ -154,8 +154,8 @@ p.data <- fd %>%
   # Removing Cabo Delgado Province due to insurgency (Confounder) &
   # filtering years above 1997 since ACLED started to record conflict events
   
- # filter( Province!= "Cabo Delgado" & Year >= 1997)
-  filter(Year >= 1997)
+  filter( Province!= "Cabo Delgado" & Year >= 1997)
+  # filter(Year >= 1997)
 
 view(p.data)
 
@@ -165,10 +165,10 @@ view(p.data)
 
 panel <- p.data %>%
   mutate(
-    Province = recode(Province,                          # recode FIRST
+    Province = recode(Province,                          
                       "Maputo City" = "Cidade de Maputo",
                       "Nassa" = "Niassa"),
-    District_Id = paste(Province, District, sep = "_")  # then create ID with clean names
+    District_Id = paste(Province, District, sep = "_")  
   )
 
 view(panel)
@@ -233,106 +233,26 @@ view(panel2)
 write.csv(panel1, "final_panel.csv")        
 
 
-  # FIXED MODEL : ANSWER TO QUESTION NUMBER ONE.
-
-fe.model <- plm(Conflict ~ Severity + Log_Displaced,
-                model = "within",effect = "twoways", data = panel2)
-summary(fe.model)
-
-modelsummary(fe.model, stars=T, statistic = c("std.error", "p.value"))
-
-  # Checking for Heteroscedasticity
-resi <- fe.model$residuals
-ggplot(data = paneld, aes(y = resi, x = Severity)) + geom_point(col = "blue") +
-  geom_abline(slope = 0)
-
-bptest(fe.model)
- # H0: Homoscedasticity
- # H1: Heteroscedasticity
- # Fail to reject the null: Homoscedasticity (Constant variance of the error term)
-
-
- # RANDOM MODEL
-ra.model <- plm(Conflict ~ Severity + Log_Displaced,
-                model = "random", effect = "twoways", data = panel1)
-summary(ra.model)
-
-modelsummary(list("FE Estimator" = fe.model, "RE Estimator" =ra.model), stars=T,
-                  statistic = c("std.error", "p.value"))
-
-
- # Running the Hausman Test
- # H0: RE Estimator is consistent 
- # H1: FE Estimator is consistent
-
-phtest(ra.model, fe.model)
-
-
- # Running Mundlak test (RE or FE ) Needs to be fixed.
-
-paneld_mundlak <- panel1 %>%
-  as.data.frame() %>%
-  group_by(District_Id) %>%
-  mutate(
-    mean_severity = mean(Severity, na.rm = TRUE),
-    mean_displaced = mean(log1p(Displaced), na.rm = TRUE)
-  ) %>%
-  ungroup()
-
-paneld_mundlak <- pdata.frame(paneld_mundlak, index = c("District_Id", "Year"))
-
-mundlak.model <- plm(
-  Conflict ~ Severity + log1p(Displaced) + mean_severity + mean_displaced,
-  data = paneld_mundlak,
-  model = "random",
-  effect = "individual"
-)
-
-summary(mundlak.model)
-
- # ANSWER TO QUESTION NUMBER TWO.
- # Understanding where exactly the flood-conflict nexus is strong
  
-panel2 <- as.data.frame(panel1) 
 
-view(panel2)
 
-table(panel2$Region, useNA = "always")
 
-head(panel2[, c("District_Id", "Year", "Region", "Conflict", "Severity")])
 
- # Regression in the North Region
-model_north <- plm(Conflict ~ Severity + Log_Displaced,
-                    data = filter(panel2, Region == 1),
-                    effect = "individual",
-                    model = "within",
-                    index = c("District_Id","Year"))
-summary(model_north)
+
+
+
+
  
- # Regression model for the Centre Region
-model_centre <- plm(Conflict ~ Severity + Log_Displaced,
-                     data = filter(panel2, Region == 2),
-                     effect = "individual",
-                     model = "within",
-                     index = c("District_Id","Year"))
-summary(model_centre)
-
- # Regression model for the South Region
-model_south <- plm(Conflict ~ Severity + Log_Displaced,
-                    data = filter(panel2, Region == 3),
-                    effect = "individual",
-                    model = "within",
-                    index = c("District_Id", "Year"))
-summary(model_south)
-
-modelsummary(list("North" = model_north, "Centre" = model_centre,
-                  "South" = model_south), stars=T,
-             statistic = c("std.error", "p.value"))
-
-write.csv(panel2, "conflict_flood.csv")
 
 
- # ANSWER TO THIRD RESEARCH QUESTION.
+
+
+
+
+ 
+ 
+
+
 
 
 
